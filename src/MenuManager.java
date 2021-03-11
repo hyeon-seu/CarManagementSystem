ccbe5174ca11e781e49b4c5517bd6569ac1721d5
hyeon-seu <skzkek33@naver.com>
2021-03-12 00:25:22 +0900

    removed hello.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Cars");
+		System.out.println("2. Delete Cars");
+		System.out.println("3. Edit Cars");
+		System.out.println("4. View Cars");
+		System.out.println("5. Exit");
+	}
+
+}
