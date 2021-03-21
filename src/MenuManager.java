a29fb4198fc0485a0787edf8b505b3364c852453
hyeon-seu <skzkek33@naver.com>
2021-03-21 22:42:16 +0900

    add menu selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,12 +1,53 @@
+import java.util.Scanner;
 
 public class MenuManager {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num = 0;
+		
+		while (num != 6) {
+		System.out.println("**** Cars Management System Menu ****");
 		System.out.println("1. Add Cars");
 		System.out.println("2. Delete Cars");
 		System.out.println("3. Edit Cars");
 		System.out.println("4. View Cars");
-		System.out.println("5. Exit");
+		System.out.println("5. Show a menu");
+		System.out.println("6. Exit");
+		System.out.print("Please Select one number between 1 - 6 : ");
+		num = input.nextInt();
+		
+		if (num == 1) {
+		System.out.print("Car NAME :");
+			  String carname = input.next();
+		System.out.print("Year of Manufacture for Car :");
+		int caryears = input.nextInt();
+		System.out.print("The price of a Car :");
+		int carprice = input.nextInt();
+		System.out.print("Maximum speed :");
+		int carspeed = input.nextInt();
+		
+		}
+		else if (num ==2) {
+			
+		}
+		else if (num ==3) {
+			
+		}
+		else if (num ==4) {
+			
+		}
+		else {
+			continue;
+		}
+		}
+		
+		
+	
+	
+	
+	
 	}
+	
 
 }
